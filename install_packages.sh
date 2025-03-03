f07fe095c147966d5f15765c57badf7c7aac0bff
Thad Turner <turner.w.thad@gmail.com>
2025-03-02 23:46:39 -0500

    Adding Librewolf Packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # Install packages from packages.list
-while read -r package
-do
-  # Instlal list of packages with Yay
-  yay -S --needed "$package"
-done < packages.list
+yay -S --needed $(cat packages.list)
 
 # Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
