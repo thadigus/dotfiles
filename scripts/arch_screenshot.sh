6156027cc934812e9d9b7ad2813b6672d7ff9a07
Thadigus <turner.w.thad@gmail.com>
2026-03-14 23:34:53 -0400

    Fixing screnshots

--- scripts/arch_screenshot.sh
+++ scripts/arch_screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p ~/Pictures/Screenshots
-grim -g "$(slurp -d)" | satty --filename - --output-filename "~/Pictures/Screenshots/screenshot-%+.png"
+grim -g "$(slurp -d)" - | satty --filename - --output-filename "~/Pictures/Screenshots/screenshot-%+.png"
