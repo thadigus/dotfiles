6c7cf87879b7c05868ddbece65c065c8de17c285
thadigus <thad@turnerservices.cloud>
2026-07-17 10:27:33 -0400

    Adding Firefox config

--- nixos/home.nix
+++ nixos/home.nix
@@ -28,6 +28,9 @@
   programs.zsh = {
     enable = true;
     initContent = builtins.readFile ../zsh/.zshrc;
+    oh-my-zsh = {
+      enable = true;
+    };
   };
   programs.tmux = {
     enable = true;
@@ -41,6 +44,41 @@
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
+    };
+  };
+  programs.firefox = {
+    enable = true;
+    languagePacks = [ "en-US" ];
+    policies = {
+      AppAutoUpdate = false;
+      BackgroundAppUpdate = false;
+      DisableFirefoxStudies = true;
+      DisableProfileIMport = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableFormHistory = true;
+      
+      DisplayMenuBar = "never";
+      DontCheckDefaultBrowser = true;
+      HardwareAcceleration = true;
+      OfferToSaveLogins = false;
+
+      ExtensionSettings = let
+        moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+      in {
+        "*".installation_mode = "blocked";
+
+        "uBlock0@raymondhill.net" = {
+          install_url       = moz "ublock-origin";
+          installation_mode = "force_installed";
+          updates_disabled  = true;
+        };
+      };
+    };
+    profiles.default.search = {
+    force           = true;
+    default         = "DuckDuckGo";
+    privateDefault  = "DuckDuckGo";
     };
   };
 }
