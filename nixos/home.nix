68f33e66b006a0cd2bbad33f944f499fbdf30dc2
thadigus <no-reply@turnerservices.cloud>
2026-07-28 12:39:38 -0400

    Adding simple wmenu config to NixOS

--- nixos/home.nix
+++ nixos/home.nix
@@ -7,6 +7,7 @@
   # home.packages is for all NixOS packages without home manager support, or without special config
   home.packages = with pkgs; [
     wl-clipboard grim slurp
+    wmenu
     swaylock swayidle swaybg
     imagemagick satty
     brightnessctl
