d1daf38209124c43689b14aaef253ac696ab7f20
thadigus <no-reply@turnerservices.cloud>
2026-08-17 16:45:28 -0400

    Making screenlock not use blur, just blurred desktop background

--- scripts/arch_screenlock.sh
+++ scripts/arch_screenlock.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+BGR="$HOME/.config/desktopwallpaper.png"
 IMG="$HOME/.config/swaylock/screenlock.png"
 DIR="$HOME/.config/swaylock/"
 
 mkdir -p "$DIR"
 
-# take screenshot
-grim - | magick convert - -scale 10% -scale 1000% -blur 0x8 "$IMG"
+if [ ! -f "$IMG" ]; then
+  magick $BGR -scale 10% -scale 1000% -blur 0x8 "$IMG"
+fi
 
 # lock screen
 swaylock -i "$IMG"
